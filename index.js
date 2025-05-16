e4d96fd64b0ba52237a2c8ae0e20bbb1cf6d71cf
vmatveev <vmatveevspam@gmail.com>
2025-05-16 18:28:20 -0400

    test

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ import 'dotenv/config';
 const app = express();
 const port = 3000;
 
-const apiKey = "openuv-4bsirmaobaubs-io";
-
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
